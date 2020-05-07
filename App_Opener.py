28dc423807a5c352c7e7a7329d1a2565d4d21f93
Serendipity18 <44249000+Serendipity18@users.noreply.github.com>
2020-05-06 22:19:00 -0400

    Add files via upload

---
+++ App_Opener.py
@@ -0,0 +1,13 @@
+'''
+Created on Oct 28, 2019
+Simple workspace opener I created but can be easily expanded upon by adding 
+more programs that you would typically open in your workspace.
+@author: Josh
+'''
+import subprocess
+
+#Function to open apps
+def OpenApplication():
+    subprocess.call('D:\\Eclipse\\eclipse\\eclipse.exe')
+
+OpenApplication()
